b143a3a76dd7413e3bff808da33dbfcc7f511add
anfernee84 <eduard.rogulin@outlook.com>
2021-11-05 14:08:57 +0200

    Update server.py

--- client-server/server.py
+++ client-server/server.py
@@ -5,6 +5,7 @@ def server_program():
     host = socket.gethostname()
     port = 8080
     server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((host, port))
     server_socket.listen(3)
     conn, address = server_socket.accept()
@@ -19,7 +20,7 @@ def server_program():
         conn.send(data.encode())
   
     conn.close()
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
  
 
 if __name__ == '__main__':
