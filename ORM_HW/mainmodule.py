34bc0ee541d5ef897d93b2c4c81bffb4e1eb1f23
anfernee84 <eduard.rogulin@outlook.com>
2021-12-01 00:21:20 +0200

    add some files

--- ORM_HW/mainmodule.py
+++ ORM_HW/mainmodule.py
@@ -10,6 +10,40 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 class AddressBook(Base):
-    __tablename__ = "addresssbook"
+    __tablename__ = "addressbook"
 
-    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    phone = Column(String)
+    email = Column(String)
+    birthday = Column(Date)
+    address = Column(String)
+
+    def __init__ (self, name, phone, email, birthday, address):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.birthday = birthday
+        self.address = address
+
+    def __repr__(self):
+        return f"{self.name}, {self.phone}, {self.email}, {self.birthday}, {self.address}"
+
+
+class Notes(Base):
+
+    __tablename__ = "note"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    note = Column(String)
+    tag = Column(String)
+
+    def __init__ (self, title, note, tag):
+        self.title = title
+        self.note = note
+        self.tag = tag
+
+    def __repr__(self):
+        return f'{self.title}, {self.note}, {self.tag}'
+
